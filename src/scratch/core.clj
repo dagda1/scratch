8b16eaf403151000440f91481a4838f3042ef577
dagda1 <dagda1@scotalt.net>
2015-01-15 17:55:17 +0000

    all-visited? func

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -1,37 +1,53 @@
 (ns scratch.core)
 
 ((fn [chain]
-   (letfn [(permutations [s]
-              (lazy-seq
-               (if (seq (next s))
-                 (apply concat (for [x s]
-                                 (map #(cons x %) (permutations (remove #{x} s)))))
-                 [s])))
+   (letfn [(debug-print [x]
+             (prn "=======================")
+             (prn (map #(prn (str % " - " (meta %)) ) x))
+             (prn "======================="))
+
            (find-match [left right]
-             (prn left)
-             (prn right)
-             (first (for [x left y right :when (= x y)] y)))
+             (let [potentail (first (for [x left y right :when (= x y)] y))]
+               (when (= ((meta right) potentail) :free)
+                 potentail)))
+
+           (record-single [col item match]
+             (assoc (vec col) (.indexOf col item) (with-meta item (assoc (meta item) match :visited ))))
+
+           (record-match [left right match coll]
+             (record-single (record-single coll left match) right match))
+
+           (all-visited? [s]
+             (every? #(= :visited %) (flatten (map (fn [i] (map (fn [x] ((meta i) x)) i)) s))))
+
+           (filter-visited [s]
+             (filter (fn [item] (not= (map #((meta item) %) item) [:visited :visited])) s))
+
+           (find-next [item coll]
+             (if (empty? coll)
+               nil
+               (if (find-match item (first coll))
+                 (first coll)
+                 (recur item (rest coll)))))
+
            (can-tour? [s]
-             (loop [[x & xs] s]
-               (if (empty? xs)
-                 true
-                 (if-let [m (find-match x (first xs))]
-                   (let [updated (assoc (vec s) (.indexOf s x) (with-meta x (assoc (meta x) m :visited )))]
-                     (prn (meta (first updated)))
-                     )
-                   (prn "not found")
-                   )
-                 )
-               ))]
-     (if (= (count chain) 0)
+             (loop [[x & xs :as coll] s]
+               (if-let [next (find-next x xs)]
+                 (let [match (find-match x next)
+                       updated (record-match x next match coll)]
+                   (if (all-visited? updated)
+                     true
+                     (let [updated-x (first (filter #(= x %) updated))
+                           updated-next (first (filter #(= next %) updated))
+                           rem (filter #(and (not= x %) (not= next %)) updated)
+                           next-list (conj (vec (conj rem updated-next)) updated-x)]
+                       (recur next-list))))
+                 false)))]
+     (if (= (count chain) 1)
        true
-       (loop [perms (permutations chain)]
-         (if (empty? perms)
-           false
-           (if (can-tour? (map #(with-meta % (zipmap % [:free :free])) (first perms)))
-             true
-             false
-             ;(recur (rest perms))
-             )))))
-   ) [[1 2] [2 3] [3 4] [4 1]])
+       (let [init (map #(with-meta % (zipmap % [:free :free])) chain)]
+         (if (can-tour? init)
+           true
+           (can-tour? (reverse init))
+           ))))) [[1 2] [2 3] [3 4] [4 1]])
 
