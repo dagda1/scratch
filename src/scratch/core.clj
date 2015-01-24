42f45eedfc08dfdfbff169bd771c68485d3715f0
dagda1 <dagda1@scotalt.net>
2015-01-24 16:28:20 +0000

    before fleury

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -1,30 +1,32 @@
 (ns scratch.core)
 
-((fn [s]
-   (letfn [(connected? [e]
-             (if (>= 1 (count e))
-               true
-               (let [v1 (ffirst e)
-                     in (for [ei e :when (= v1 (second ei))] (first ei))
-                     out (for [ei e :when (= v1 (first ei))] (second ei))
-                     el (filter (partial not= v1) (concat in out))
-                     ]
-                 (prn "=========")
-                 (prn (remove #(some #{v1} %) e))
-                 (prn v1)
-                 (prn (concat in out))
-                 (prn el)
-                 (prn e)
-                 (prn "=========")
-                 (if (empty? el)
-                   false
-                   (recur (remove #(some #{v1} %) e))
-                   )
-                 )
-               )
-             )]
-     (connected? s)
-     )
-   )#{[:a :b] [:b :c] [:c :d]
-               [:x :y] [:d :a] [:b :e]})
+(def graph #{[:a :b] [:b :c] [:c :d]
+               [:x :y] [:d :a] [:b :e]} )
 
+((fn [g]
+   (letfn [
+    (connected? [e]
+      (prn (count e))
+      (prn e)
+      (if (>= 1 (count e))
+        true
+        (let [
+              v1  (ffirst e)
+              ; incoming neighbours
+              in  (for [ei e :when (= v1 (second ei))] (first  ei))
+              ; outgoing neigbours
+              out (for [ei e :when (= v1 (first  ei))] (second ei))
+              e1  (filter #(not= % v1) (concat in out))]
+          (prn "=======")
+          (prn v1)
+          (prn in)
+          (prn out)
+          (prn (concat in out))
+          (prn e1)
+          (prn "=======")
+          (if (empty? e1)
+            false
+            (recur (remove #(some #{v1} %) e))))))
+    ]
+    (connected? g))
+   )graph)
