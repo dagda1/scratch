686389f8b3621f98f4ba5a2be07ef188bdfb74ca
dagda1 <dagda1@scotalt.net>
2015-01-25 20:47:48 +0000

    graph connectivity solution

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -1,34 +1,17 @@
 (ns scratch.core)
 
-(def g #{[:a :b] [:b :c] [:c :d]
-              [:x :y] [:d :a] [:b :e] [:x :a]})
+(def g #{[1 2]})
 
 ((fn [g]
    (letfn [(connected? [g]
              (loop [q (conj [] (ffirst g)) visited #{}]
                (if (empty? q)
-                 (let [rem (filter #(not (% visited)) (flatten (for [e g] e)))]
+                 (let [rem (filter #(not (contains? visited %)) (flatten (for [e g] e)))]
                    (prn rem)
                    (= 0 (count rem)))
                  (let [v1 (peek q)
                        edges (filter (partial some #{v1}) g)
                        vertices (filter (partial not= v1) (flatten edges))
-                       unvisited (filter #(not (% visited)) vertices)
-                       ]
-                   (prn "===========")
-                   (prn v1)
-                   (prn q)
-                   (prn edges)
-                   (prn vertices)
-                   (prn visited)
-                   (prn unvisited)
-                   (prn "===========")
-                   (recur (into (rest q) unvisited) (into (conj visited v1) unvisited))
-                   ))
-               )
-             )
-           ]
-     (if (= (count g) 0)
-       true
-      (connected? g)))
-   ) g )
+                       unvisited (filter #(not (contains? visited %)) vertices)]
+                   (recur (into (rest q) unvisited) (into (conj visited v1) unvisited))))))]
+     (connected? g))) g )
