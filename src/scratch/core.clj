1dd1555d6c1710e7c83a866e7302342e97b429ee
dagda1 <dagda1@scotalt.net>
2015-06-19 05:20:56 +0100

    chocolate success

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -4,30 +4,52 @@
 (defn numberify [str]
   (vec (map read-string (str/split str #" "))))
 
+(defn take-while+1 [pred coll]
+    (lazy-seq (when-let [[x & xs] (seq coll)]
+                (if (pred x)
+                  (cons x (take-while+1 pred xs)) (list x)))))
+
+(defn trade [acc bought wrappers]
+  (let [traded (int (Math/floor (/ bought wrappers)))
+        remainder (if (= 0 traded)
+                    0
+                    (- bought (* traded wrappers)))
+        result (+ traded remainder)
+        ]
+    ;; (prn "===============")
+    ;; (prn (str "acc = " acc))
+    ;; (prn (str "bought = " bought))
+    ;; (prn (str "wrappers = " wrappers))
+    ;; (prn (str "traded = ") traded)
+    ;; (prn (str "remainder = " remainder))
+    ;; (prn (str "result = " result))
+    ;; (prn (str "cunt = " (< result wrappers)))
+    ;; (prn "===============")
+    (if (< result wrappers)
+      (+ acc traded)
+      (trade (+ acc traded) result wrappers)
+      )
+    )
+  )
+
+; work out how many chocs are bought by dividing cash by amount (/ 6 2)
+; exchange wrappers by dividing bought by wrappers (/ 3 2)
+; store remaining
+; exchange wrappers + remaining for new wrappers
+; repeat until you can't exchange any more wrappers
 (defn process [[cash amount wrappers]]
   (let [bought (int (Math/floor (/ cash amount)))
-        free (int (Math/floor (/ bought wrappers)))
-        remaining (rem bought wrappers)
-        total (int (Math/floor (+ bought free)))
-        next (int (Math/floor (/ (+ free remaining) wrappers)))]
-
-    (prn "==========")
-    (prn (str "bought " bought))
-    (prn (str "free " (rem bought wrappers)))
-    (prn (str "total " total))
-    (prn (str "wrappers " wrappers))
-    (prn (str "next " next))
-    (prn "==========")
-    (+ next total)))
+        remaining (rem cash amount)]
+    (trade bought bought wrappers)))
 
 (defn print-result [[x & xs]]
   (prn x)
   (if (seq xs)
     (recur xs)))
 
-(let [input "1\n43203 60 5"
+(let [input "1\n77984 530 67091"
       lines (str/split-lines input)
       length (read-string (first lines))
       inputs (map numberify (rest lines))]
-  (print-result (map process inputs)))
+  (prn (map process inputs)))
 ; "1\n43203 60 5" => 899
