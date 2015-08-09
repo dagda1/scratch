f140a715281c9135beb681b3e23e8bfc599bb73f
dagda1 <dagda1@scotalt.net>
2015-08-09 20:38:53 +0100

    park big tree

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -6,17 +6,17 @@
 
 (defrecord TreeNode [val left right])
 
-(defn build-tree [[l r] rst node]
-  (let [left (if (= - 1 l) nil l)
-        right (if (= -1 r) nil r)]
+(defn build-tree [node coll]
+  (if-let [[x & xs] coll]
     
-    )
-  )
-
+    node))
 
 ; (assoc node :left (make-tree left))
 
-
+; { :val val
+;   :left (vec-to-tree left)
+;   ;right (vec-to-tree)}
+;
 ;                  1
 ;                 / \
 ;                2   3
@@ -25,16 +25,17 @@
 ;             /    /
 ;            /    /\
 ;           6    7  8
-;           \      / \
-;            9    10 11
+;            \     / \
+;             9   10 11
 ;
 ; x =  [2 3]
-; xs = 
-(let [input "11\n2 3\n4 -1\n5 -2\n6 -1\n7 8\n-1 9\n-1 -1\n10 11\n-1 -1\n-1 -1\n-1 -1"
+; xs = ([4 -1] [5 -1] [6 -1] [7 8] [-1 9] [-1 -1] [10 11] [-1 -1] [-1 -1] [-1 -1])
+;
+(let [input "11\n2 3\n4 -1\n5 -1\n6 -1\n7 8\n-1 9\n-1 -1\n10 11\n-1 -1\n-1 -1\n-1 -1"
       lines (str/split-lines input)
       tl (read-string (first lines))
       tree-lines (map numberify (drop 1 (take (inc tl) lines)))
       [x & xs] tree-lines
       ]
-  (build-tree x xs (TreeNode. 1 nil nil))
+  (build-tree (TreeNode. 1 nil nil) tree-lines)
   )
