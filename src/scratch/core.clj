1c3dad0c9053adbe6d71734554d0be0369dfe9b1
dagda1 <dagda1@scotalt.net>
2015-08-11 21:57:23 +0100

    basic tree construction and traversal

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -6,16 +6,42 @@
 
 (defrecord TreeNode [val left right])
 
-(defn build-tree [node coll]
-  (if-let [[x & xs] coll]
-    
-    node))
+(defn append-to-tree [node x]
+  (reduce (fn [t b]
+            (let [index (if (= b :left) 0 1)
+                  val (nth x index)]
+              (if-not (= val -1)
+                (assoc t b (TreeNode. val nil nil))
+                t
+                ))
+            ) node [:left :right]))
 
+(defn build-tree [node curr rst depth]
+  (let [t (append-to-tree node)])
+  )
+
+; process [2, 3]
+; pop 2 on stack
+; 2 gets  [4, -1]
+; pop on stack
+; 3 gets  [5, -1]
+; pop 4 on stack
+; 4 gets  [6, -1]
+; pop 5 on stack
+; 5 gets [7, 8]
+; pop 6 on stack
+; 6 gets [-1, 9]
+; pop 7 on stack
+; 7 gets [-1, -1]
+; pop 8 on stack
+; 8 gets [10, 11]
+; pop 9 on stack
+; 9 gets [-1 -1]
+; pop 10 on stack
+; 10 gets [-1 -1]
+; pop 11 on stack
+; 11 gets [-1 -1]
 ; (assoc node :left (make-tree left))
-
-; { :val val
-;   :left (vec-to-tree left)
-;   ;right (vec-to-tree)}
 ;
 ;                  1
 ;                 / \
@@ -27,15 +53,9 @@
 ;           6    7  8
 ;            \     / \
 ;             9   10 11
-;
-; x =  [2 3]
-; xs = ([4 -1] [5 -1] [6 -1] [7 8] [-1 9] [-1 -1] [10 11] [-1 -1] [-1 -1] [-1 -1])
-;
 (let [input "11\n2 3\n4 -1\n5 -1\n6 -1\n7 8\n-1 9\n-1 -1\n10 11\n-1 -1\n-1 -1\n-1 -1"
       lines (str/split-lines input)
       tl (read-string (first lines))
       tree-lines (map numberify (drop 1 (take (inc tl) lines)))
-      [x & xs] tree-lines
-      ]
-  (build-tree (TreeNode. 1 nil nil) tree-lines)
-  )
+      tree (build-tree (TreeNode. 1 nil nil) (first tree-lines) (rest tree-lines) 1)]
+  (prn tree))
