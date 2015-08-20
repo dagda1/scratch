5716528c7b325cdf6118ebfddea847165a46b884
dagda1 <dagda1@scotalt.net>
2015-08-20 21:39:49 +0100

    map tree input

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -6,77 +6,20 @@
 
 (defrecord TreeNode [val left right])
 
-(defn tree-map [idx itm]
-  (let [side (if (= 0 idx) :left :right)]
-    {:val itm :side side :index idx}))
+(defn preprocess-input [n xs]
+  (let [source (map vector (range 1 n) xs)]
+    (into {} (map (fn [[k v]]
+                    {k v}
+                    ) source))
+    )
+  )
 
-(defn append-to-tree [node x]
-  (reduce (fn [t b]
-            (let [index (if (= b :left) 0 1)
-                  val (nth x index)]
-              (if-not (= val -1)
-                (assoc t b (TreeNode. val nil nil))
-                t))
-            ) node [:left :right]))
 
-(defn build-tree
-  [node xs]
-  (let [process-branch (fn process-branch [[counter t'] l]
-                         (if-not (= (:val l) -1)
-                           (let [next-branch (nth xs counter)]
-                             (prn "=============")
-                             (prn l)
-                             (prn counter)
-                             (prn next-branch)
-                             (prn "=============")
-                             [(inc counter) t'])
-                           [counter t']))
-        mark-branch (fn mark-branch [x]
-                      (map-indexed tree-map x))
-        [counter tree] (reduce (fn [[counter tree] x]
-                                 (reduce process-branch
-                                         [counter tree]
-                                         (mark-branch x)))
-                               [1 node] xs)]
-    tree))
-
-; process [2, 3]
-; pop 2 on stack
-; 2 gets  [4, -1]
-; pop on stack
-; 3 gets  [5, -1]
-; pop 4 on stack
-; 4 gets  [6, -1]
-; pop 5 on stack
-; 5 gets [7, 8]
-; pop 6 on stack
-; 6 gets [-1, 9]
-; pop 7 on stack
-; 7 gets [-1, -1]
-; pop 8 on stack
-; 8 gets [10, 11]
-; pop 9 on stack
-; 9 gets [-1 -1]
-; pop 10 on stack
-; 10 gets [-1 -1]
-; pop 11 on stack
-; 11 gets [-1 -1]
-; (assoc node :left (make-tree left))
-;
-;                  1
-;                 / \
-;                2   3
-;               /    /
-;              4    5
-;             /    /
-;            /    /\
-;           /    /  \
-;           6   7    8
-;            \      / \
-;             \    /   \
-;              9  10   11
 (let [input "11\n2 3\n4 -1\n5 -1\n6 -1\n7 8\n-1 9\n-1 -1\n10 11\n-1 -1\n-1 -1\n-1 -1"
       lines (str/split-lines input)
       tl (read-string (first lines))
       tree-lines (map numberify (drop 1 (take (inc tl) lines)))
-      tree (build-tree (TreeNode. 1 nil nil) tree-lines)])
+      tree-source (preprocess-input tl tree-lines)
+      ]
+  (prn tree-source)
+  )
